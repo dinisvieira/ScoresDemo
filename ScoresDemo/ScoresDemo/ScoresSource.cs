c0abedb05804a167dc990ffd3b1d01b2089ff887
Dinis Vieira <dinisvieira@outlook.com>
2016-09-27 12:27:44 +0100

    minor changes to core

--- ScoresDemo/ScoresDemo/ScoresSource.cs
+++ ScoresDemo/ScoresDemo/ScoresSource.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -14,26 +9,62 @@ namespace ScoresDemo
 {
     public class ScoresSource
     {
+        private static string endpoint = "https://api.crowdscores.com/api/v1"; //API Endpoint
 
-        private static string endpoint = "https://api.crowdscores.com/api/v1";
-        private static string lastMatches = "/matches?competition_id=2";
-
-        public static async Task<ObservableCollection<Match>>  GetLastCompetitionMatches(string competitionId)
+        //Get all the competitions available on the API
+        public static async Task<ObservableCollection<Competition>> GetCompetitions()
         {
             var client = new HttpClient();
 
             //Header required to call the service (API key)
             client.DefaultRequestHeaders.Add("x-crowdscores-api-key", "91e57182c3954dc68a2e184c30271b4a");
 
-            //number of days to fetch
+            //build request URI
+            var requestUri = endpoint + "/competitions";
+
+            //Calls the service and returns a json string
+            string response = await client.GetStringAsync(requestUri);
+
+            //Converts json string into a ObservableCollection of JObject
+            var jsonCompetitionsList = JsonConvert.DeserializeObject<ObservableCollection<JObject>>(response);
+
+            //Build list of match objects based on json string
+            var competitionsList = new ObservableCollection<Competition>();
+            foreach (JObject competition in jsonCompetitionsList)
+            {
+                JToken regionToken;
+                competition.TryGetValue("region", out regionToken);
+
+                var newCompetition = new Competition()
+                {
+                    Id = competition.Value<int>("dbid"),
+                    Name = competition.Value<string>("name"),
+                    RegionName = regionToken.Value<string>("name")
+                };
+                competitionsList.Add(newCompetition);
+            }
+
+            return competitionsList;
+        }
+
+        //Get the last matches of a given competition (id)
+        public static async Task<ObservableCollection<Match>> GetLastCompetitionMatches(string competitionId)
+        {
+            var client = new HttpClient();
+
+            //Header required to call the service (API key)
+            client.DefaultRequestHeaders.Add("x-crowdscores-api-key", "91e57182c3954dc68a2e184c30271b4a");
+
+            //number of days to fetch (always fetching the last 7 days)
             var daysToFetch = DateTime.Now.AddDays(-7);
 
+            //build request URI
             var requestUri = endpoint + "/matches?competition_id=" + competitionId + "&from=" + daysToFetch.ToString("yyyy-MM-ddThh:mm:ss");
 
             //Calls the service and returns a json string
             string response = await client.GetStringAsync(requestUri);
 
-            //Converts json string into a ObservableCollection of strings
+            //Converts json string into a ObservableCollection of JObject
             var jsonMatchesList = JsonConvert.DeserializeObject<ObservableCollection<JObject>>(response);
             
             //Build list of match objects based on json string
@@ -68,41 +99,6 @@ namespace ScoresDemo
             }
 
             return matchList;
-        }
-
-        public static async Task<ObservableCollection<Competition>> GetCompetitions()
-        {
-            var client = new HttpClient();
-
-            //Header required to call the service (API key)
-            client.DefaultRequestHeaders.Add("x-crowdscores-api-key", "91e57182c3954dc68a2e184c30271b4a");
-
-            var requestUri = endpoint + "/competitions";
-
-            //Calls the service and returns a json string
-            string response = await client.GetStringAsync(requestUri);
-
-            //Converts json string into a ObservableCollection of strings
-            var jsonCompetitionsList = JsonConvert.DeserializeObject<ObservableCollection<JObject>>(response);
-
-            //Build list of match objects based on json string
-            var competitionsList = new ObservableCollection<Competition>();
-            foreach (JObject competition in jsonCompetitionsList)
-            {
-
-                JToken regionToken;
-                competition.TryGetValue("region", out regionToken);
-
-                var newCompetition = new Competition()
-                {
-                    Id = competition.Value<int>("dbid"),
-                    Name = competition.Value<string>("name"),
-                    RegionName = regionToken.Value<string>("name")
-                };
-                competitionsList.Add(newCompetition);
-            }
-
-            return competitionsList;
         }
     }
 }
